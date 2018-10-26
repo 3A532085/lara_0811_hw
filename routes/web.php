d0f17f0d0ffb2fb8437833b80ebce9e08929a423
江珮妤 <Luxgen077@gmail.com>
2018-10-26 19:22:56 +0800

    修改Route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/about', ['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/news', ['as'=>'news.index','uses'=>'NewsController@index']);
